b8aed223c98eb8b6699a5b09c65238cdcf5ac4f7
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2021-12-23 15:48:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import time 
+from pyrogram import Cilent
+
+print(
+    "Pyrogram String Session Generator For Telegram Bots\n\nEnter Your Details From my.telegram.org\n\n
+    )
+    
+    with Cilent(":memory:",
+    api_id=int(input("Enter API_ID: "))),
+    api hash=input("Enter API_HASH: ")) as WhiteDevil
+    
+    session = """
+    <b>Your String Session ;<b>
+    
+    <code>{}</code>
+    
+    👽 <b>Dont Share This With Anyone Else!</b> 👽
+    
+    Developer : @WhiteDevilOp999
+    """.format(TroJanz.export_session_string())
+    
+    print("\n\nGenerating String Session...\n\n")
+    
+    TroJanz.send_message("me",session, parse_mode="html")
+    
+    time.sleep(1)
+    
+    print(
+        "Your String Have Been Sent To Your Telegram Saved Messages\n\n@WhiteDevilOp999")
+        
